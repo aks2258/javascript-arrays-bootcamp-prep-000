7036acd0b7f72202a47a9148827a6f953edd9604
Arnie Serrano <arniekidd@gmail.com>
2020-06-14 02:31:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(chocolateBars, butterfinger){
-  return 
   chocolateBars=["butterfinger", ...chocolateBars]
+  return chocolateBars
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, butterfinger){
-  return 
   chocolateBars.unshift("butterfinger")
+  return chocolateBars
 }
