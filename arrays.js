c2a4f11432977400cdb80f7ab1fc8b844d66b6ef
Arnie Serrano <arniekidd@gmail.com>
2020-06-14 02:26:57 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(){
+  return 
+  chocolateBars=["almond Joy", ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  return 
+  chocolateBars.unshift("almond Joy")
+}
